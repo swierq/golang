cf8e277c7763ee38d47a0033a8cbdc52ffa3024d
Przemyslaw Swierczek <swierq@gmail.com>
2025-01-04 14:43:01 +0100

    Added context support in webapp.Serve method
    closes #23

--- cmd/loadek/main.go
+++ cmd/loadek/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
 
 	"github.com/posener/cmd"
 	"github.com/swierq/golang/internal/loadek"
@@ -19,13 +23,16 @@ func main() {
 	port := root.Int("port", 8080, "Listen Port")
 	_ = root.Parse()
 
-	config := webapp.NewConfig(webapp.WithPort(uint16(*port)))
+	config := webapp.NewConfig(webapp.WithPort(uint16(*port)), webapp.WithLogLevel("info"))
 	app := &loadekApp{webapp: webapp.NewApp(config)}
 
+	ctx, cancel := context.WithCancel(context.Background())
+
 	app.webapp.Router.HandleFunc("GET /cpuload/{number}", app.cpuLoad)
-	err := app.webapp.Serve()
+	go waitOnSignal(cancel)
+	err := app.webapp.Serve(ctx)
 	if err != nil {
-		panic(err)
+		app.webapp.Logger.Error("Webserver problem", "error", err)
 	}
 }
 
@@ -47,4 +54,11 @@ func (app *loadekApp) cpuLoad(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		app.webapp.LogError(r, err)
 	}
+}
+
+func waitOnSignal(cancel context.CancelFunc) {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	cancel()
 }
