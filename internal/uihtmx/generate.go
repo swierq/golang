bfdc8dd3a2b483f52954cf433d03864810b14d25
Przemyslaw Swierczek <swierq@gmail.com>
2025-03-26 17:23:19 +0100

    Updating deps (#36)

--- internal/uihtmx/generate.go
+++ internal/uihtmx/generate.go
@@ -1,5 +1,5 @@
 package uihtmx
 
-//go:generate go run github.com/a-h/templ/cmd/templ@v0.3.833 generate
+//go:generate go run github.com/a-h/templ/cmd/templ generate
 
 //go:generate tailwindcss -c ./tailwind.config.js -i ./ui/assets/css/input.css -o ./ui/assets/css/tailwind.css --minify
